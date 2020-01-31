bf31935bda45eb9db7046787b94ac053582fd889
cartermak <cartermak3@gmail.com>
2020-01-31 00:55:02 -0700

    SD+Serial Working on Teensy, Load Cell Untested

--- DAQ/src/main.cpp
+++ DAQ/src/main.cpp
@@ -1,54 +1,127 @@
 #include <Arduino.h>
 #include <HX711.h>
-#include <Servo.h>
 #include <SparkFunMAX31855k.h> // Using the max31855k driver
-#include <SPI.h>  // Included here too due Arduino IDE; Used in above header
+#include <SdFat.h>
+
+// Deprecated includes
+// #include <Servo.h>
+// #include <SPI.h>  // Included here too due Arduino IDE; Used in above header
 
 // HX711 library: https://github.com/bogde/HX711
 // this is also in the lib manager - authors Bogdan Necula, Andreas Motl
 
-// #define calibration_factor 386050.00 // kg
-#define calibration_factor 1280 //Nnbvx
+// ================================= HARDWARE DEFINITION =================================
+
+// #define CALIBRATION_FACTOR 386050.00 // kg
+#define CALIBRATION_FACTOR 1280 //Nnbvx
 //I calibrated this using random stuff i had lying around (207g battery pack, 120g solenoid, 3kg metal stock)
 //it's not great. I'm also wondering if we should calibrate it using known loads that are close to what we expect to be measuring?
 
-#define LOADCELL_DOUT_PIN  3
-#define LOADCELL_SCK_PIN  2
+// Define loadcell pins
+#define LOADCELL_DATA_PIN  29
+#define LOADCELL_SCK_PIN  30
 
+// Define SPI power pins
+#define SPI_VCC 16
+#define SPI_GND 17
 
-HX711 loadCell;
-SparkFunMAX31855k probe1(10);
-SparkFunMAX31855k probe2(9);
-SparkFunMAX31855k probe3(8);
+// Define Analog pin for pressure transducer
+int pressurePin = A21;
 
-void setup() {
-  Serial.begin(9600);
-  loadCell.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  loadCell.set_scale(calibration_factor);
-  loadCell.tare(); // zero at startup
-}
+// Define temperature probe chip select pins
+const uint8_t probeCS[] = {35,36,37};
 
-void loop() {
-  float temperature1 = probe1.readTempK();
-  float temperature2 = probe2.readTempK();
-  float temperature3 = probe3.readTempK();
-  double Pressure = 14400;
-  Pressure = Pressure * analogRead(A0);
-  Pressure = Pressure - 1507000;
-  logLoad(Pressure, temperature1, temperature2, temperature3);
-}
+// ================================= CLASS/VARIABLE DECLARATIONS =================================
 
+// Initialize sensors
+HX711 LoadCell;
+SparkFunMAX31855k probe0(probeCS[0],SPI_VCC,SPI_GND);
+SparkFunMAX31855k probe1(probeCS[1],SPI_VCC,SPI_GND);
+SparkFunMAX31855k probe2(probeCS[2],SPI_VCC,SPI_GND);
 
-void logLoad(double Pressure, float temp1, float temp2, float temp3){
+// Declare temperature vars
+float temp0,temp1,temp2;
+
+// Float to store pressure
+float pressure;
+
+// Float to store load cell data
+float force;
+
+// SD Card Initialization
+SdFatSdio SD;
+File of;
+
+// ================================= FUNCITON DEFINITIONS =================================
+
+void serialPrinter(double pressure, float temp0, float temp1, float temp2, float force){
   Serial.print(millis());
   Serial.print(", ");
-  Serial.print(Pressure);
+  Serial.print(pressure);
+  Serial.print(", ");
+  Serial.print(temp0);
   Serial.print(", ");
   Serial.print(temp1);
   Serial.print(", ");
   Serial.print(temp2);
   Serial.print(", ");
-  Serial.print(temp3);
-  Serial.print(", ");
-  Serial.println(loadCell.get_units(), 5); //returns a float. 2nd param is # of decimal places to display/record. In grams the 1st dec is definitely unreliable. 
+  Serial.println(force, 5); //returns a float. 2nd param is # of decimal places to display/record. In grams the 1st dec is definitely unreliable. 
+};
+
+void sdPrinter(double pressure, float temp0, float temp1, float temp2, float force) {
+  of = SD.open("datalog.dat", FILE_WRITE);
+
+  of.print(millis());
+  of.print(", ");
+  of.print(pressure);
+  of.print(", ");
+  of.print(temp0);
+  of.print(", ");
+  of.print(temp1);
+  of.print(", ");
+  of.print(temp2);
+  of.print(", ");
+  of.println(force, 5); //returns a float. 2nd param is # of decimal places to display/record. In grams the 1st dec is definitely unreliable. 
+
+  of.close();
+}
+
+void setup() {
+
+  // Start Serial comms
+  Serial.begin(9600);
+
+  // Initialize load cell
+  LoadCell.begin(LOADCELL_DATA_PIN, LOADCELL_SCK_PIN);
+  LoadCell.set_scale(CALIBRATION_FACTOR);
+  LoadCell.tare(); // zero at startup
+
+  // Init SD Card
+  if (!SD.begin()) {Serial.println("WARNING || SD Card NOT INITIATED");}
+  of = SD.open("datalog.dat", FILE_WRITE);
+  of.println("TIME, PRESSURE, TEMP0, TEMP1, TEMP2, FORCE");
+  of.close();
+  
+}
+
+void loop() {
+
+  // Sample thermocouples
+  temp0 = probe0.readTempK();
+  temp1 = probe1.readTempK();
+  temp2 = probe2.readTempK();
+
+  // Sample pressure transducer
+  pressure = 14400;
+  pressure = pressure * analogRead(pressurePin);
+  pressure = pressure - 1507000;
+
+  // Sample load cell
+  force = LoadCell.get_units();
+
+  // Serial print and SD write
+  serialPrinter(pressure, temp0, temp1, temp2, force);
+  sdPrinter(pressure, temp0, temp1, temp2, force);
+
+  // delay(500);
 }
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # DAQ
+
 Code for Static Test Stand DAQ
+
+## Teensy Pin Assignemnts
+
+| Teensy Pin |                  Target                   |
+| :--------: | :---------------------------------------: |
+| 11 (MISO0) |    Thermocouple Boards SO (Serial Out)    |
+| 13 (SCK0)  |  Thermocouple Boards SCK (Serial Clock)   |
+|     16     |             Thermocouple VCC              |
+|     17     |             Thermocouple GND              |
+| {35,36,37} | Thermocouple Chip Selects (one per board) |
+|     29     |           Load Cell Data ("DT")           |
+|     30     |          Load Cell Clock ("SCK")          |
+|    A13     |        Pressure Transducer Signal         |
+
+## Load Cell Wiring
+
+| Wire Color | M12 Connector Pin Number | Description  |
+| :--------: | :----------------------: | :----------: |
+|    Red     |            1             | Excitation + |
+|   Black    |            2             | Excitation - |
+|   White    |            3             |   Signal -   |
+|   Green    |            4             |   Signal +   |
+
+## Resources
+
+[Teensy Pinout](https://www.pjrc.com/teensy/card9a_rev1.pdf)
+[Thermocouple Breakout Documentation](https://learn.sparkfun.com/tutorials/max31855k-thermocouple-breakout-hookup-guideZ)
+[Load Cell Amp Documentation](https://learn.sparkfun.com/tutorials/load-cell-amplifier-hx711-breakout-hookup-guide/all) *Note: description is for a different breakout board, but uses the same SOC.*
+[Load Cell Connector Pinout](https://ptglobal-cdn.s3.ap-southeast-2.amazonaws.com/file_asset_store/fileasset/300/file/39e91befa7.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJEAX2FF3ZMX66G3Q%2F20200131%2Fap-southeast-2%2Fs3%2Faws4_request&X-Amz-Date=20200131T051506Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=243de9526c2ff692fd20c8bb86888d24aea5ec9f95a52d6aeea2c118842127e3)
