85ef2a258cf036bce183e4316c72fc6f197fb6d7
Carter Mak <cartermak3@gmail.com>
2020-05-11 17:48:55 -0600

    Updated Teensy for 3 transducers

--- DAQ/src/main.cpp
+++ DAQ/src/main.cpp
@@ -13,7 +13,8 @@
 // ================================= HARDWARE DEFINITION =================================
 
 // #define CALIBRATION_FACTOR 386050.00 // kg
-#define CALIBRATION_FACTOR 1280 //Nnbvx
+// #define CALIBRATION_FACTOR 1280 //Nnbvx
+#define CALIBRATION_FACTOR 1
 //I calibrated this using random stuff i had lying around (207g battery pack, 120g solenoid, 3kg metal stock)
 //it's not great. I'm also wondering if we should calibrate it using known loads that are close to what we expect to be measuring?
 
@@ -25,8 +26,8 @@
 #define SPI_VCC 16
 #define SPI_GND 17
 
-// Define Analog pin for pressure transducer
-int pressurePin = A21;
+// Define Analog pins for pressure transducer
+const int pressurePin[] = {A12,A13,A14};
 
 // Define temperature probe chip select pins
 const uint8_t probeCS[] = {35,36,37};
@@ -43,21 +44,21 @@ SparkFunMAX31855k probe2(probeCS[2],SPI_VCC,SPI_GND);
 float temp0,temp1,temp2;
 
 // Float to store pressure
-float pressure;
+double pressure1,pressure2,pressure3;
 
 // Float to store load cell data
 float force;
 
-// SD Card Initialization
-SdFatSdio SD;
-File of;
-
 // ================================= FUNCTION DEFINITIONS =================================
 
-void serialPrinter(double pressure, float temp0, float temp1, float temp2, float force){
+void serialPrinter(double pressure1, double pressure2, double pressure3, float temp0, float temp1, float temp2, float force){
   Serial.print(millis());
   Serial.print(", ");
-  Serial.print(pressure);
+  Serial.print(pressure1, 5);
+  Serial.print(", ");
+  Serial.print(pressure2, 5);
+  Serial.print(", ");
+  Serial.print(pressure3, 5);
   Serial.print(", ");
   Serial.print(temp0);
   Serial.print(", ");
@@ -67,25 +68,6 @@ void serialPrinter(double pressure, float temp0, float temp1, float temp2, float
   Serial.print(", ");
   Serial.println(force, 5); //returns a float. 2nd param is # of decimal places to display/record. In grams the 1st dec is definitely unreliable. 
 };
-
-void sdPrinter(double pressure, float temp0, float temp1, float temp2, float force) {
-  
-  if (of = SD.open("datalog.dat", FILE_WRITE)) {
-    of.print(millis());
-    of.print(", ");
-    of.print(pressure);
-    of.print(", ");
-    of.print(temp0);
-    of.print(", ");
-    of.print(temp1);
-    of.print(", ");
-    of.print(temp2);
-    of.print(", ");
-    of.println(force, 5);
-
-    of.close();
-  }
-}
 
 void setup() {
 
@@ -97,11 +79,6 @@ void setup() {
   LoadCell.set_scale(CALIBRATION_FACTOR);
   LoadCell.tare(); // zero at startup
 
-  // Init SD Card
-  if (!SD.begin()) {Serial.println("WARNING || SD Card NOT INITIATED");}
-  of = SD.open("datalog.dat", FILE_WRITE);
-  of.println("TIME, PRESSURE, TEMP0, TEMP1, TEMP2, FORCE");
-  of.close();
 }
 
 void loop() {
@@ -112,16 +89,18 @@ void loop() {
   temp2 = probe2.readTempK();
 
   // Sample pressure transducer
-  pressure = 14400;
-  pressure = pressure * analogRead(pressurePin);
-  pressure = pressure - 1507000;
+  // pressure = 14400;
+  // pressure = pressure * analogRead(pressurePin);
+  // pressure = pressure - 1507000;
+  pressure1 = analogRead(pressurePin[0]);
+  pressure2 = analogRead(pressurePin[1]);
+  pressure3 = analogRead(pressurePin[2]);
 
   // Sample load cell
   force = LoadCell.get_units();
 
   // Serial print and SD write
-  serialPrinter(pressure, temp0, temp1, temp2, force);
-  sdPrinter(pressure, temp0, temp1, temp2, force);
+  serialPrinter(pressure1, pressure2, pressure3, temp0, temp1, temp2, force);
 
-  delay(10);
+  // delay(10);
 }
