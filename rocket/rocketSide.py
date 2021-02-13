c6ecb949f100f71db4bd39c97b126e30f020b518
LyonFoster <lyfo8327@colorado.edu>
2021-02-12 20:32:08 -0700

    more changesss

--- rocket/rocketSide.py
+++ rocket/rocketSide.py
@@ -7,41 +7,62 @@ import time
 
 class rocketSideTalk(): 
     
-    def __init__(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as talkSocket:
+    def __init__(self, rocketIP, rocketPORT, pilotIP, pilotPORT): #constructor
+        self.talkSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.rocketAddress = (rocketIP, rocketPORT)
+        self.pilotAddress = (pilotIP, pilotPORT)
 
-            message = None
-            
-            talkSocket.sendto()
 
 
     def run(self):
         while True:
-        lstng = "\rtalking"
-        elip = "."
-        for i in range(4):
-            sys.stdout.write(lstng + ((i) * elip))
-            time.sleep(1)
-            sys.stdout.write("\r             ")
-            sys.stdout.flush
+
+            message = b"<message from rocket>"
+            self.talkSocket.sendto(message, self.pilotAddress)
+            time.sleep(.5)
+
+
+
+
+
+    # def printTalking(self): #place holder function
+    #         lstng = "\rtalking"
+    #         elip = "."
+    #         for i in range(4):
+    #             sys.stdout.write(lstng + ((i) * elip))
+    #             time.sleep(1)
+    #             sys.stdout.write("\r             ") #line ends
+    #             sys.stdout.flush
 
 
 class rocketSideListen():
     
 
+    def __init__(self, rocketIP, rocketPORT, pilotIP, pilotPORT):
+        self.listenSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.rocketAddress = (rocketIP, rocketPORT)
+        self.pilotAddress = (pilotIP, pilotPORT)
+
+        self.listenSocket.bind(self.rocketAddress)
+
 
     def run(self):
+
         while True:
-            lstng = "\rlistening"
-            elip = "."
-            for i in range(4):
-                sys.stdout.write(lstng + ((i) * elip))
-                time.sleep(1)
-                sys.stdout.write("\r             ")
-                sys.stdout.flush
 
+            data = self.listenSocket.recv(1024)
+            print("recieved from pilot: %s" % data)
 
+            
 
-bruh = rocketSideListen()
+        
+    # def run(self): #place holder function
+    #     while True:
+    #         lstng = "\rlistening"
+    #         elip = "."
+    #         for i in range(4):
+    #             sys.stdout.write(lstng + ((i) * elip))
+    #             time.sleep(1)
+    #             sys.stdout.write("\r             ") #line ends
+    #             sys.stdout.flush
 
-bruh.run()
