782c72724a89430f1f66df44bd8dd4f96dccef07
Ian Faber <ian.m.faber@gmail.com>
2021-03-16 23:14:03 -0600

    Refactored spiceShuttleJSON into two separate JSON files for easier ingestion, need a better way of identifying the files (i.e. not hard drive based)

--- pilot/pilotModule/JSONParsing.py
+++ pilot/pilotModule/JSONParsing.py
@@ -1,14 +1,25 @@
 import json
 
+## no more than 2 lists in json
 
+class Configuration():
 
-def ingestJSON(filename):
-    with open(filename) as f:
-        spiceShuttleJSON = json.load(f)
+    def __init__(self,filename):
+        self.filename = filename
 
-    return spiceShuttleJSON
+    def ingestJSON(self,filename):
+        with open(self.filename) as f:
+            RawJSON = json.load(f) 
+        
+        return RawJSON
 
+IOConfig = Configuration('c:/Users/ianmf/Documents/SRLDAQRepo/configurations/IO.json')
+ButtonConfig = Configuration('c:/Users/ianmf/Documents/SRLDAQRepo/configurations/buttons.json')
 
-# spiceShuttleDict = ingestJSON('spiceShuttle.json')
+IODict = IOConfig.ingestJSON('IO.json')
+ButtonDict = ButtonConfig.ingestJSON('buttons.json')
+
+print(IODict['inputs'][2]['name'])
+print(ButtonDict[1]['name'])
 
 # print([spiceShuttleDict['devices'][i]['address'] for i in range(len(spiceShuttleDict['devices'])) if spiceShuttleDict['devices'][i]['type'] == 'ADS1115Ducer'])
