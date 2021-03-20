dd2b35efa762c0a1b430d94d49de6f1db0d087f0
Ian Faber <ian.m.faber@gmail.com>
2021-03-19 21:06:55 -0600

    Fully implemented thermocouple parsing, next steps include parsing everything else and adding different OS support

--- pilot/pilotModule/JSONParsing.py
+++ pilot/pilotModule/JSONParsing.py
@@ -1,25 +1,53 @@
 import json
 
-## no more than 2 lists in json
-
 class Configuration():
 
-    def __init__(self,filename):
+    def __init__(self, filename):
         self.filename = filename
-
-    def ingestJSON(self,filename):
-        with open(self.filename) as f:
-            RawJSON = json.load(f) 
+        self.rawJSON = dict()
+        self.thermocoupleDict = dict()
+        self.ducerDict = dict()
         
-        return RawJSON
+    def ingestJSON(self):
+        with open(self.filename) as f:
+            self.rawJSON = json.load(f)
+        
+    def thermocouples(self):
+        for i in range(len(self.rawJSON['inputs'])):
+            if self.rawJSON['inputs'][i]['type'] == "ADS1115Thermo":
+                self.thermocoupleDict[i] = self.rawJSON['inputs'][i]
+        return self.thermocoupleDict
 
+    def ducers(self):
+        for i in range(len(self.rawJSON['inputs'])):
+            if self.rawJSON['inputs'][i]['type'] == "ADS1115Ducer":
+                self.ducerDict[i] = self.rawJSON['inputs'][i]
+        return self.ducerDict
+
+    def run(self):
+        self.thermocoupleDict = self.thermocouples()
+        self.ducerDict = self.ducers()
+ 
 IOConfig = Configuration('c:/Users/ianmf/Documents/SRLDAQRepo/configurations/IO.json')
-ButtonConfig = Configuration('c:/Users/ianmf/Documents/SRLDAQRepo/configurations/buttons.json')
+buttonConfig = Configuration('c:/Users/ianmf/Documents/SRLDAQRepo/configurations/buttons.json')
 
-IODict = IOConfig.ingestJSON('IO.json')
-ButtonDict = ButtonConfig.ingestJSON('buttons.json')
+IOConfig.ingestJSON()
+IOConfig.run()
 
-print(IODict['inputs'][2]['name'])
-print(ButtonDict[1]['name'])
+for i in range(len(IOConfig.thermocoupleDict)):
+    print(IOConfig.thermocoupleDict[i]['name'])
+
+# for i in range(len(IOConfig.ducerDict)):
+#     print(IOConfig.ducerDict[i]['name'])
+
+# print(IODict['inputs'][2]['name'])
+# print(ButtonDict[1]['name'])
 
 # print([spiceShuttleDict['devices'][i]['address'] for i in range(len(spiceShuttleDict['devices'])) if spiceShuttleDict['devices'][i]['type'] == 'ADS1115Ducer'])
+# from sys import platform
+# if platform == "linux" or platform == "linux2":
+#     # linux
+# elif platform == "darwin":
+#     # OS X
+# elif platform == "win32":
+#     # Windows...
